7c02a2a17f51ab406c746e4c8b81f13ca2836410
kevinelong <kevinelong@gmail.com>
2022-11-03 10:53:43 -0700

    rolling rocks problem

---
+++ demos/rolling_rocks.js
@@ -0,0 +1,24 @@
+/*
+rolling rocks
+Rules:
+    Rocks roll left to right.
+    Walls do not roll.
+    Walls stop rocks from rolling.
+    
+    O is a rock
+    X is a wall
+    . is empty space
+
+    The world is a series of symbols that change over time.
+    Time loops through a series of cycles until they stop changing (become stable). (at most 8)
+
+    here are some initial inputs and final output (we are omitting the states in between)
+    "O......X" becomes "......OX"
+    ".O.X.O.X" becomes "..OX..OX"
+    "O....XO." becomes "....OX.."
+*/
+function roll(initial){
+    final = initial
+    //...
+    return final
+}
